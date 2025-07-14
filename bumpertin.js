4c10c2941fc147e5ed52518dfbd28b7ffdcd79c6
fatpepol <markusbjorgeziegler@hotmail.com>
2025-07-14 15:49:10 +0200

    type your functions correctly kids

--- bumpertin.js
+++ bumpertin.js
@@ -1,9 +1,12 @@
 const cartwheel = () => {
   const declan = 'i will now do a cartwheel';
+  console.log(declan);
   const execution = 'KYAGHHH';
+  console.log(execution);
   const result = 'hey i did it';
+  console.log(result);
 
   return declan + '<br>' + execution + '<br>' + result;
 };
-
+console.log(cartwheel());
 export { cartwheel };
--- jumpertin.js
+++ jumpertin.js
@@ -1,9 +1,13 @@
-const bartwheel = () => {
+function bartwheel() {
   const declan = "human i remember you're genocides";
+  console.log(declan);
   const execution = 'duduDUDU';
+  console.log(execution);
   const result = "human i rmemembr you're reset";
+  console.log(result);
 
   return declan + '<br>' + execution + '<br>' + result;
 };
 
+console.log(bartwheel());
 export { bartwheel };
